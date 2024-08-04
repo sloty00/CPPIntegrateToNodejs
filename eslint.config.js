44c7b4783d0d3505d62185294345a6d3bb260b81
Jose Vargas Oyarzun <jvoyarzun81@gmail.com>
2024-08-04 14:46:46 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,18 @@
-
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    // Agrega tus reglas personalizadas aquí
+  },
+};
