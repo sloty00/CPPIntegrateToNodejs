3678d5723914a30cddbf7bcd3358567acc758c15
Jose Vargas Oyarzun <jvoyarzun81@gmail.com>
2024-08-04 15:00:54 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,30 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
+/** @type {import('eslint').FlatConfig} */
+const { rules: reactRules } = require('eslint-plugin-react');
+
+module.exports = [
+  {
+    languageOptions: {
+      globals: {
+        browser: true,
+        es2021: true,
+      },
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      react: require('eslint-plugin-react'),
+    },
+    rules: {
+      // Configuración correcta para boolean-prop-naming
+      'react/boolean-prop-naming': ['error', {
+        "rule": "^(is|has)[A-Z]([A-Za-z0-9]?)+",
+        "message": "Prop name `{{propName}}` doesn’t match rule `{{pattern}}`"
+      }],
+      // Otras reglas de React
+      'no-console': 'warn',
+      'no-unused-vars': 'warn',
+    },
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  rules: {
-    // Agrega tus reglas personalizadas aquí
-  },
-};
+];
